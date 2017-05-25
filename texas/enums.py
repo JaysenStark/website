df29b2b57a878b5b29b47d8e01d8e3956fa668ff
JaysenStark <838459914@qq.com>
2017-05-26 03:37:09 +0800

    modify enums.py, add support for round addition

--- texas/enums.py
+++ texas/enums.py
@@ -2,9 +2,12 @@ from enum import IntEnum
 
 class ActionType(IntEnum):
 
+    EMPTY = -1
     CALL = 0
     RAISE = 1
     FOLD = 2
+    NEXT_ROUND = 3
+
 
 class Round(IntEnum):
     PREFLOP = 0
@@ -12,12 +15,22 @@ class Round(IntEnum):
     TURN = 2
     RIVER = 3
 
+    def __add__(self, other):
+        return Round(self.value + other)
+
+
+class CardType(IntEnum):
+    HOLE = 0
+    PUBLIC = 1
+
 
 ActionNames = {
+    ActionType.NEXT_ROUND : "NEXT_ROUND",
     ActionType.CALL : "CALL",
     ActionType.RAISE : "RAISE",
     ActionType.FOLD : "FOLD",
 }
+
 
 RoundNames = {
     Round.PREFLOP : "PREFLOP",
@@ -25,4 +38,11 @@ RoundNames = {
     Round.TURN : "TURN", 
     Round.RIVER : "RIVER", 
 }
+
+
+CardNames = {
+    CardType.HOLE : "HOLE",
+    CardType.PUBLIC : "PUBLIC",
+}
+
 
